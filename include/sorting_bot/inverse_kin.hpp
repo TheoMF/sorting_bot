f8dfe7d152f8a083683bfad42906d6f4dc8ee49b
Théo Martinez <theo.martinezf@gmail.com>
2025-10-20 13:47:05 +0000

    put IK from planner in another class

---
+++ include/sorting_bot/inverse_kin.hpp
@@ -0,0 +1,59 @@
+#include "sorting_bot/inverse_kin_base.hpp"
+
+#include "pinocchio/parsers/urdf.hpp"
+#include "pinocchio/multibody/sample-models.hpp"
+#include "pinocchio/algorithm/joint-configuration.hpp"
+#include "pinocchio/algorithm/model.hpp"
+#include "pinocchio/algorithm/frames.hpp"
+
+class InverseKin : public InverseKinBase{
+
+public:
+    std::tuple<Eigen::VectorXd, double> get_inverse_kinematics_for_des_pose(Eigen::VectorXd q_init, const pinocchio::SE3 &in_world_M_des_pose)
+    {
+        Eigen::VectorXd q = q_init;
+        pinocchio::Data::Matrix6x J(6, model_ptr_->nv);
+        J.setZero();
+
+        bool success = false;
+        typedef Eigen::Matrix<double, 6, 1> Vector6d;
+        typedef Eigen::Matrix<double, 5, 1> Vector4d;
+        Vector6d err;
+        Eigen::VectorXd v(model_ptr_->nv);
+        for (int i = 0;; i++)
+        {
+            pinocchio::framesForwardKinematics(*model_ptr_, *data_ptr_, q);
+            const pinocchio::SE3 iMd = data_ptr_->oMf[ee_frame_id_].actInv(in_world_M_des_pose);
+            err = pinocchio::log6(iMd).toVector();
+            err(3) *= 0.001;
+            err(5) *= 0.2;
+            if (err.norm() < eps)
+            {
+                success = true;
+                break;
+            }
+            if (i >= IT_MAX)
+            {
+                success = false;
+                break;
+            }
+            pinocchio::computeFrameJacobian(*model_ptr_, *data_ptr_, q, ee_frame_id_, pinocchio::ReferenceFrame::LOCAL, J);
+            pinocchio::Data::Matrix6 Jlog;
+            pinocchio::Jlog6(iMd.inverse(), Jlog);
+            J = -Jlog * J;
+            pinocchio::Data::Matrix6 JJt;
+            JJt.noalias() = J * J.transpose();
+            JJt.diagonal().array() += damp;
+            v.noalias() = -J.transpose() * JJt.ldlt().solve(err);
+            q = pinocchio::integrate(*model_ptr_, q, v * DT);
+        }
+        set_q_in_joint_limits(q);
+        return std::make_tuple(q, err.norm());
+    }
+private:
+    // Inverse kinematics parameters
+    const double eps = 1e-3;
+    const int IT_MAX = 20000;
+    const double DT = 1e-1;
+    const double damp = 1e-6;
+};
--- include/sorting_bot/motion_planner.hpp
+++ include/sorting_bot/motion_planner.hpp
@@ -12,7 +12,8 @@
 #include "pinocchio/algorithm/frames.hpp"
 
 #include "sorting_bot/quintic_polynom.hpp"
-#include "sorting_bot/genetic_algo_inv_kin.hpp"
+#include "sorting_bot/genetic_algo_inverse_kin.hpp"
+#include "sorting_bot/inverse_kin.hpp"
 
 class MotionPlanner
 {
@@ -33,17 +34,18 @@ public:
         nq_ = model_ptr_->nq;
         data_ptr_ = std::make_shared<pinocchio::Data>(data);
         ee_frame_id_ = model_ptr_->getFrameId(ee_frame_name_);
-        genetic_algo_inv_kin_.initialize(model_ptr_,data_ptr_,ee_frame_id_);
+        genetic_algo_inverse_kin_.initialize(model_ptr_,data_ptr_,ee_frame_id_);
+        inverse_kin_.initialize(model_ptr_,data_ptr_,ee_frame_id_);
     }
 
     void initialize_population(const pinocchio::SE3 &in_world_M_des_pose)
     {
-        genetic_algo_inv_kin_.initialize_population(in_world_M_des_pose);
+        genetic_algo_inverse_kin_.initialize_population(in_world_M_des_pose);
     }
 
     Individual run_gen_algo(const pinocchio::SE3 &in_world_M_des_pose)
     {
-        return genetic_algo_inv_kin_.run_gen_algo(in_world_M_des_pose);
+        return genetic_algo_inverse_kin_.run_gen_algo(in_world_M_des_pose);
     }
 
     pinocchio::SE3 get_frame_pose_at_q(Eigen::VectorXd &q, std::string &frame_name)
@@ -53,17 +55,6 @@ public:
         return data_ptr_->oMf[frame_id];
     }
 
-    void set_q_in_joint_limits(Eigen::VectorXd &q)
-    {
-        for (int joint_idx = 0; joint_idx < model_ptr_->nq; joint_idx++)
-        {
-            if (q[joint_idx] < model_ptr_->lowerPositionLimit[joint_idx])
-                q[joint_idx] = model_ptr_->lowerPositionLimit[joint_idx];
-            if (q[joint_idx] > model_ptr_->upperPositionLimit[joint_idx])
-                q[joint_idx] = model_ptr_->upperPositionLimit[joint_idx];
-        }
-    }
-
     bool is_initialized()
     {
         return quintic_polynom_.is_initialized();
@@ -71,44 +62,7 @@ public:
 
     std::tuple<Eigen::VectorXd, double> get_inverse_kinematics_for_des_pose(Eigen::VectorXd q_init, const pinocchio::SE3 &in_world_M_des_pose)
     {
-        Eigen::VectorXd q = q_init;
-        pinocchio::Data::Matrix6x J(6, model_ptr_->nv);
-        J.setZero();
-
-        bool success = false;
-        typedef Eigen::Matrix<double, 6, 1> Vector6d;
-        typedef Eigen::Matrix<double, 5, 1> Vector4d;
-        Vector6d err;
-        Eigen::VectorXd v(model_ptr_->nv);
-        for (int i = 0;; i++)
-        {
-            pinocchio::framesForwardKinematics(*model_ptr_, *data_ptr_, q);
-            const pinocchio::SE3 iMd = data_ptr_->oMf[ee_frame_id_].actInv(in_world_M_des_pose);
-            err = pinocchio::log6(iMd).toVector();
-            err(3) *= 0.001;
-            err(5) *= 0.2;
-            if (err.norm() < eps)
-            {
-                success = true;
-                break;
-            }
-            if (i >= IT_MAX)
-            {
-                success = false;
-                break;
-            }
-            pinocchio::computeFrameJacobian(*model_ptr_, *data_ptr_, q, ee_frame_id_, pinocchio::ReferenceFrame::LOCAL, J);
-            pinocchio::Data::Matrix6 Jlog;
-            pinocchio::Jlog6(iMd.inverse(), Jlog);
-            J = -Jlog * J;
-            pinocchio::Data::Matrix6 JJt;
-            JJt.noalias() = J * J.transpose();
-            JJt.diagonal().array() += damp;
-            v.noalias() = -J.transpose() * JJt.ldlt().solve(err);
-            q = pinocchio::integrate(*model_ptr_, q, v * DT);
-        }
-        set_q_in_joint_limits(q);
-        return std::make_tuple(q, err.norm());
+        return inverse_kin_.get_inverse_kinematics_for_des_pose(q_init,in_world_M_des_pose);
     }
 
     void set_plan(const Eigen::VectorXd &q_start, const std::vector<Eigen::VectorXd> &q_waypoints)
@@ -128,14 +82,6 @@ private:
     int ee_frame_id_;
     int nq_;
     QuinticPolynom quintic_polynom_;
-    GeneticAlgoInvKin  genetic_algo_inv_kin_;
-
-    
-
-    // Inverse kinematics parameters
-    const double eps = 1e-3;
-    const int IT_MAX = 20000;
-    const double DT = 1e-1;
-    const double damp = 1e-6;
-
+    GeneticAlgoInverseKin  genetic_algo_inverse_kin_;
+    InverseKin inverse_kin_;
 };
