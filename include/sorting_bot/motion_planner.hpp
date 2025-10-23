6c517361fcc95b6c47017fa78165dd5e3a6d6e21
Théo Martinez <theo.martinezf@gmail.com>
2025-10-23 12:52:51 +0000

    format planner code, remove unnecessary function

--- include/sorting_bot/motion_planner.hpp
+++ include/sorting_bot/motion_planner.hpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "pinocchio/parsers/urdf.hpp"
 
 #include "sorting_bot/quintic_polynom.hpp"
@@ -7,70 +9,80 @@
 class MotionPlanner
 {
 public:
-    MotionPlanner()
-    {
-    }
-    void initialize(std::string filename, std::string ee_frame_name)
-    {
-        pinocchio::Model model;
-        pinocchio::urdf::buildModel(filename, model);
-        std::vector<pinocchio::JointIndex> list_of_joints_to_lock_by_id{model.getJointId("gripper")};
-        Eigen::VectorXd q_rand = pinocchio::randomConfiguration(model);
-        model = pinocchio::buildReducedModel(model, list_of_joints_to_lock_by_id, q_rand);
-        model_ptr_ = std::make_shared<pinocchio::Model>(model);
-        ee_frame_name_ = ee_frame_name;
-        pinocchio::Data data(*model_ptr_);
-        nq_ = model_ptr_->nq;
-        data_ptr_ = std::make_shared<pinocchio::Data>(data);
-        ee_frame_id_ = model_ptr_->getFrameId(ee_frame_name_);
-        genetic_algo_inverse_kin_.initialize(model_ptr_,data_ptr_,ee_frame_id_);
-        inverse_kin_.initialize(model_ptr_,data_ptr_,ee_frame_id_);
-    }
+  MotionPlanner()
+  {
+  }
+  void initialize(std::string filename, std::string ee_frame_name)
+  {
+    pinocchio::Model model;
+    pinocchio::urdf::buildModel(filename, model);
+    std::vector<pinocchio::JointIndex> list_of_joints_to_lock_by_id{model.getJointId("gripper")};
+    Eigen::VectorXd q_rand = pinocchio::randomConfiguration(model);
+    model = pinocchio::buildReducedModel(model, list_of_joints_to_lock_by_id, q_rand);
+    model_ptr_ = std::make_shared<pinocchio::Model>(model);
+    ee_frame_name_ = ee_frame_name;
+    pinocchio::Data data(*model_ptr_);
+    nq_ = model_ptr_->nq;
+    data_ptr_ = std::make_shared<pinocchio::Data>(data);
+    ee_frame_id_ = model_ptr_->getFrameId(ee_frame_name_);
+    genetic_algo_inverse_kin_.initialize(model_ptr_, data_ptr_, ee_frame_id_);
+    inverse_kin_.initialize(model_ptr_, data_ptr_, ee_frame_id_);
+  }
 
-    void initialize_population(const pinocchio::SE3 &in_world_M_des_pose)
+  Eigen::VectorXd get_inverse_kinematic_at_pose(const Eigen::VectorXd &q_init, const pinocchio::SE3 &des_transform)
+  {
+    Eigen::VectorXd q_inv_kin;
+    std::tuple<Eigen::VectorXd, double> inv_kin_res = inverse_kin_.get_inverse_kinematics_for_des_pose(q_init, des_transform);
+    double pose_err = std::get<1>(inv_kin_res);
+    if (pose_err > 0.01)
     {
-        genetic_algo_inverse_kin_.initialize_population(in_world_M_des_pose);
+      std::cout << "IK didn't converge, Run genetic algorithm" << std::endl;
+      Individual best = genetic_algo_inverse_kin_.run_gen_algo(des_transform);
+      q_inv_kin = best.q();
     }
+    else
+    {
+      q_inv_kin = std::get<0>(inv_kin_res);
+    }
+    std::string ee_frame_name = "gripper_frame_link";
+    pinocchio::SE3 pose_inv_kin = get_frame_pose_at_q(q_inv_kin, ee_frame_name);
+    Eigen::Matrix<double, 6, 1> err = pinocchio::log6(pose_inv_kin.actInv(des_transform)).toVector();
+    Eigen::Matrix<double, 5, 1> err_5d;
+    err_5d.head<3>() = err.head<3>();
+    err_5d.tail<2>() = err.tail<2>();
+    std::cout << "inverse kinematic pose error norm " << err_5d.norm() << "base IK err " << pose_err << std::endl;
+    return q_inv_kin;
+  }
 
-    Individual run_gen_algo(const pinocchio::SE3 &in_world_M_des_pose)
-    {
-        return genetic_algo_inverse_kin_.run_gen_algo(in_world_M_des_pose);
-    }
+  pinocchio::SE3 get_frame_pose_at_q(Eigen::VectorXd &q, std::string &frame_name)
+  {
+    pinocchio::framesForwardKinematics(*model_ptr_, *data_ptr_, q);
+    auto frame_id = model_ptr_->getFrameId(frame_name);
+    return data_ptr_->oMf[frame_id];
+  }
 
-    pinocchio::SE3 get_frame_pose_at_q(Eigen::VectorXd &q, std::string &frame_name)
-    {
-        pinocchio::framesForwardKinematics(*model_ptr_, *data_ptr_, q);
-        auto frame_id = model_ptr_->getFrameId(frame_name);
-        return data_ptr_->oMf[frame_id];
-    }
+  void set_plan(const Eigen::VectorXd &q_start, const std::vector<Eigen::VectorXd> &q_waypoints)
+  {
+    quintic_polynom_.set_plan(q_start, q_waypoints);
+  }
 
-    bool is_initialized()
-    {
-        return quintic_polynom_.is_initialized();
-    }
+  Eigen::VectorXd get_configuration_at_t(const double &time)
+  {
+    return quintic_polynom_.get_configuration_at_t(time);
+  }
 
-    std::tuple<Eigen::VectorXd, double> get_inverse_kinematics_for_des_pose(Eigen::VectorXd q_init, const pinocchio::SE3 &in_world_M_des_pose)
-    {
-        return inverse_kin_.get_inverse_kinematics_for_des_pose(q_init,in_world_M_des_pose);
-    }
-
-    void set_plan(const Eigen::VectorXd &q_start, const std::vector<Eigen::VectorXd> &q_waypoints)
-    {
-        quintic_polynom_.set_plan(q_start,q_waypoints);
-    }
-
-    Eigen::VectorXd get_configuration_at_t(const double &time)
-    {
-        return quintic_polynom_.get_configuration_at_t(time);
-    }
+  double get_traj_duration()
+  {
+    return quintic_polynom_.traj_duration();
+  }
 
 private:
-    std::shared_ptr<pinocchio::Model> model_ptr_;
-    std::shared_ptr<pinocchio::Data> data_ptr_;
-    std::string ee_frame_name_;
-    int ee_frame_id_;
-    int nq_;
-    QuinticPolynom quintic_polynom_;
-    GeneticAlgoInverseKin  genetic_algo_inverse_kin_;
-    InverseKin inverse_kin_;
+  std::shared_ptr<pinocchio::Model> model_ptr_;
+  std::shared_ptr<pinocchio::Data> data_ptr_;
+  std::string ee_frame_name_;
+  int ee_frame_id_;
+  int nq_;
+  QuinticPolynom quintic_polynom_;
+  GeneticAlgoInverseKin genetic_algo_inverse_kin_;
+  InverseKin inverse_kin_;
 };
